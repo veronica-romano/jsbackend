050151c1578d865c55d00fe41bb6c474fea1f251
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-08-29 15:02:14 -0300

    Update conversoes.js

--- aulas/conversoes.js
+++ aulas/conversoes.js
@@ -9,3 +9,18 @@ console.log(numero == numeroString); //conversão implícita
 = atribuir
 == comparar valores
 === comparar valores e tipos */
+
+const numeroString2 = "456";
+
+const numeroString3 = Number("456");
+
+const numeroString4 = Number("456a");
+
+//conversão explícita
+console.log(numero + Number(numeroString))
+
+console.log(numero + numeroString2)
+
+console.log(numero + numeroString3)
+
+console.log(numero + numeroString4)
