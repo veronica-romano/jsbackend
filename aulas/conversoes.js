3ea89adfb5f9a84fbf92185f3b6111bf1514f7ec
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-08-29 15:09:28 -0300

    conversão para string

--- aulas/conversoes.js
+++ aulas/conversoes.js
@@ -23,4 +23,8 @@ console.log(numero + numeroString3)
 
 console.log(numero + numeroString4)
 
+//conversão explícita para string
+
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
 
